d0fbc9a82a440d896588b60078a00b1793167d04
an3wers <an3wers@gmail.com>
2022-03-11 13:58:59 +0500

    finish basic funcs

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+nav {
+  display: flex;
+  padding: 1rem;
+}
+
+nav > a:not(:last-child) {
+  margin-right: 1rem;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 0.5rem;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="home">
+    <app-select
+      :placeholder="placeholder"
+      :url="URL_DATA"
+    ></app-select>
+  </div>
+</template>
+
+<script>
+import AppSelect from '@/components/AppSelect.vue'
+import { ref, reactive, computed } from 'vue'
+
+export default {
+  setup() {
+
+    const URL_DATA = 'https://jsonplaceholder.typicode.com/users'
+
+    const placeholder = 'Open this select menu'
+    
+    return {
+      placeholder,
+      URL_DATA
+    }
+  },
+
+  components: {
+    AppSelect
+  }
+}
+</script>
